ded141708d85d9d8bef421ca36244f04a9b5ded1
RichardMizak <richard.mizak@kosickaakademia.sk>
2021-04-16 11:04:12 +0200

    Initial commit

--- src/main/java/sk/kosickaakademia/sk/scratchpad/database/Database.java
+++ src/main/java/sk/kosickaakademia/sk/scratchpad/database/Database.java
@@ -1,17 +1,30 @@
 package sk.kosickaakademia.sk.scratchpad.database;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import sk.kosickaakademia.sk.scratchpad.util.Tasks;
+
+import java.util.Date;
 import java.util.List;
 
 
 public class Database implements Mongo {
 //dbname: TaskDB
 //collection: Tasks
+private static final MongoClient mongoClient = new MongoClient();
+    private static MongoDatabase database;
+    private static Document docs;
+    private static MongoCollection<Document> collection;
+    private static Date date = new Date();
     @Override
     public void insertTask(Tasks task) {
-
+        database=mongoClient.getDatabase("TaskDB");
+        collection = database.getCollection("Tasks");
+        docs=new Document("task",task);
+        collection.insertOne(docs);
     }
-
     @Override
     public void setTaskToDone(int id) {
 
