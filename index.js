077666a90148943c96740965190ff67ef63f3827
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-04-26 21:11:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -61,6 +61,19 @@ app.post('/inserttask',(req,res)=> {
             } 
         })
     })
+ app.updateOne('/updatetask',(req,res)=> {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+      
+        dbo.collection("customers").updateOne(myquery, newvalues, function(err, res) {
+          if (err) throw err;
+          console.log("1 document updated");
+          db.close();
+        });
+      });
+    })
+
 app.listen(3000, ()=>{
     console.log('3000');
 })
