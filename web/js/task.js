fa207acfa2b6b243e06751ae0e4e6a4dfc1241bb
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-04-29 23:41:50 +0200

    Add files via upload

---
+++ web/js/task.js
@@ -0,0 +1,31 @@
+$(()=>{
+    $.ajax({
+        url:"http://localhost:3000/task/show",
+        type: "get",     
+        statusCode:{
+            200: (result) => {
+                for(var index in result){
+                    const id = result[index]._id;
+                    const name = result[index].name;
+                    const priority = result[index].priority;
+                    const date = result[index].date;
+                    const done = result[index].done;
+                    const price = result[index].price;
+                    let text = "<b>" + name + "</b>" + " (" + date + ") <br>";
+                    text = text +  "<b> Priority </b>: " + priority;
+                    if(price >= 0){
+                        text = text + "<b> Price: </b>" + price+"€";
+                    }
+                    text = text + "<b> Done: </b>" + done;
+                    $("#mainContainer").append("<div>" +"<div class='center'>" + text + "</div>" + "</div> <br>");
+                }
+            },
+            400: (err) => {
+                console.log('Bad request');
+            },
+            404: (err) => {
+                console.log('Not found');
+            }
+        }
+    })
+})
